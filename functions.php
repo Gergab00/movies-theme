e48d4180a7ec595ee8d27c3867868828a31287c9
Gerardo Gonzalez <gergab00@hotmail.com>
2023-01-11 23:36:54 -0600

    change: some letters

--- functions.php
+++ functions.php
@@ -7,9 +7,9 @@
  * @package movies_theme
  */
 
-if ( ! defined( '_S_VERSION' ) ) {
+if ( ! defined( 'MOVIES_THEME_VERSION' ) ) {
 	// Replace the version number of the theme on each release.
-	define( '_S_VERSION', '1.0.0' );
+	define( 'MOVIES_THEME_VERSION', '1.0.0' );
 }
 
 /**
